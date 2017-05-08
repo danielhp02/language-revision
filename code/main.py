d93e19e9403341e00c6195f5ac843c0ea3b47faf
Daniel <danielhp@optusnet.com.au>
2017-05-08 16:40:45 +1000

    moved to json, added catergories

--- code/main.py
+++ code/main.py
@@ -1,62 +1,71 @@
 # -*- coding: utf-8 -*-
 
 from random import randint, shuffle
-import pickle
+import json
 import os
 import sys
 
 import objects
 
 # if __file__ == 'main.py':
-words_filepath = 'words.p'
+words_filepath = 'words.json'
 
 words = []
 
 def load_words():
     global words
     try: # to load words
-        with open(words_filepath, 'rb') as f:
-            words = pickle.load(f)
+        with open(words_filepath, 'r') as input:
+            word_dict = json.load(input)["words"]
+            for w in word_dict:
+                word = add_word(w["english"], w["german"], w["french"], w["germanGender"], w["frenchGender"])
     except FileNotFoundError: # Create file if not found
         print("Failed to find file '", words_filepath, "'.")
-        with open(words_filepath, 'wb'):
-            pass
+        with open(words_filepath, 'w'):
+            json.dump({"words": []}, output, -1, indent=2)
 
 def save_words():
-    with open(words_filepath, 'wb') as output:
-        pickle.dump(words, output, -1)
+    global words
+    word_json = []
+    for w in words:
+        word_json.append(w.__dict__)
+    with open(words_filepath, 'w') as output:
+        json.dump({"words": word_json}, fp=output, indent=2)
 
 def check_word_exists(word, language=None):
     global words
-    if language == 'english':
-        for w in words:
-            if w.english == word:
-                return True
-        return False
-    elif language == 'german':
-        for w in words:
-            if w.german == word:
-                return True
-        return False
-    elif language == 'french':
-        for w in words:
-            if w.french == word:
-                return True
-        return False
+    if words != []:
+        if language == 'english':
+            for w in words:
+                if w.english == word:
+                    return True
+            return False
+        elif language == 'german':
+            for w in words:
+                if w.german == word:
+                    return True
+            return False
+        elif language == 'french':
+            for w in words:
+                if w.french == word:
+                    return True
+            return False
+        else:
+            for w in words:
+                if w.english == word:
+                    return True
+                if w.german == word:
+                    return True
+                if w.french == word:
+                    return True
+            return False
     else:
-        for w in words:
-            if w.english == word:
-                return True
-            if w.german == word:
-                return True
-            if w.french == word:
-                return True
         return False
 
 def add_word(english, german, french, germanGender, frenchGender):
     global words
-    check_word_exists(english)
-    words.append(objects.Word(english, german, french, germanGender, frenchGender))
+    # check_word_exists(english)
+    words.append(objects.Word(english, german, french, germanGender, frenchGender, catergory))
 
 score = 0
 
@@ -77,8 +86,9 @@ while running:
         french = input("What is the word in French? ").lower()
         germanGender = input("What is the gender of that word in German? ").lower()
         frenchGender = input("What is the gender of that word in French? ").lower()
+        catergory = list(input("What catergory is this in? ").lower())
 
-        add_word(english, german, french, germanGender, frenchGender)
+        add_word(english, german, french, germanGender, frenchGender, catergory)
 
     elif action == 'save words':
         save_words()
