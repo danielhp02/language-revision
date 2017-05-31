e1feb8b807651fce61b75638634f9db761cf7560
Daniel <danielhp@optusnet.com.au>
2017-05-31 22:02:34 +1000

    fixed bug

--- code/main.py
+++ code/main.py
@@ -21,14 +21,15 @@ def load_words():
         for ws in sets_dict:
             noun_list = []
             if len(ws["nouns"]) > 0:
-                print(len(ws["nouns"]))
+                print("hey")
                 for n in ws["nouns"]:
-                    noun_list.append(add_noun(n["english"], n["language"], n['translation'], n["gender"], topic=None))
+                    noun_list.append(add_noun(n["english"], n["language"], n['translation'], n["gender"]))
                     print("hey")
             verb_list = []
             if len(ws["verbs"]) > 0:
+                print("eh")
                 for v in ws["verbs"]:
-                    verb_list.append(add_noun(v["english"], v["language"], v['translation'], v["pastParticiple"], v["auxVerb"]))
+                    verb_list.append(add_verb(v["english"], v["language"], v['translation'], v["pastParticiple"], v["auxVerb"]))
                     print("v")
             add_set(ws["language"], ws["topic"], noun_list, verb_list)
             print(ws["topic"])
@@ -112,11 +113,11 @@ def print_words_in_set(language, topic):
     if aSet is not None:
         print("Nouns:")
         for n in aSet.nouns:
-            print(n)#n.english, '-', n.translation)
+            print(n.translation, '-', n.english)
             print("hey")
         print("Verbs:")
         for v in aSet.verbs:
-            print(v)#v.english, '-', v.translation)
+            print(v.translation, '-', v.english)
 
 def coloured(text, colour, bold=False):
     reset = '\u001b[0m'
@@ -159,46 +160,50 @@ load_words()
 quiz = objects.Quiz(coloured)
 
 running = True
-while running:
-    action = input(coloured("What would you like to do? ", 'blue', True)).lower()
-    if action == 'add word':
-        english = input(coloured("What is the word in English? ", 'magenta', True)).lower()
-        language = input(coloured("What language? ", 'magenta', True)).lower()
-        translation = input(coloured("What is the word in " + language + "? ", 'magenta', True)).lower()
-        topic = input(coloured("What topic is this word in? ", 'magenta', True)).lower()
+try:
+    while running:
+        action = input(coloured("What would you like to do? ", 'blue', True)).lower()
+        if action == 'add word':
+            english = input(coloured("What is the word in English? ", 'magenta', True)).lower()
+            language = input(coloured("What language? ", 'magenta', True)).lower()
+            translation = input(coloured("What is the word in " + language + "? ", 'magenta', True)).lower()
+            topic = input(coloured("What topic is this word in? ", 'magenta', True)).lower()
 
-        type = input(coloured("What type of word is this? (noun/verb) ", 'magenta', True)).lower()
-        if type == 'noun':
-            gender = input(coloured("What is the gender of this word? ", 'magenta', True)).lower()
+            type = input(coloured("What type of word is this? (noun/verb) ", 'magenta', True)).lower()
+            if type == 'noun':
+                gender = input(coloured("What is the gender of this word? ", 'magenta', True)).lower()
 
-            add_noun(english, language, translation, gender, topic)
-            print(coloured("Noun added to topic '" + topic + "'.", 'magenta'))
-        elif type == 'verb':
-            pastParticiple = input(coloured("What is the past participle of this word? ", 'magenta', True)).lower()
-            auxVerb = input(coloured("What auxiliary verb does this word use? ", 'magenta', True)).lower()
+                add_noun(english, language, translation, gender, topic)
+                print(coloured("Noun added to topic '" + topic + "'.", 'magenta'))
+            elif type == 'verb':
+                pastParticiple = input(coloured("What is the past participle of this word? ", 'magenta', True)).lower()
+                auxVerb = input(coloured("What auxiliary verb does this word use? ", 'magenta', True)).lower()
 
-            add_verb(english, language, translation, pastParticiple, auxVerb, topic)
-            print(coloured("Verb added to topic '" + topic + "'.", 'magenta'))
+                add_verb(english, language, translation, pastParticiple, auxVerb, topic)
+                print(coloured("Verb added to topic '" + topic + "'.", 'magenta'))
 
-    elif action.split()[0] == 'quiz': # needs language, type and topic
-        if len(action.split()) in [1,2,3]:
-            print(coloured("More info is needed to start a quiz.", 'yellow'))
-        elif action.split()[1] == 'french':
-            if action.split()[2] == 'verbs':
-                quiz.verbs(findSet('french', action.split()[3].lower()))
-            elif action.split()[2] == 'nouns':
-                quiz.nouns(findSet('french', action.split()[3].lower()))
-        elif action.split()[1] == 'german':
-            if action.split()[2] == 'verbs':
-                quiz.verbs(findSet('german', action.split()[3].lower()))
-            elif action.split()[2] == 'nouns':
-                quiz.nouns(findSet('german', action.split()[3].lower()))
+        elif action.split()[0] == 'quiz': # needs language, type and topic
+            if len(action.split()) in [1,2,3]:
+                print(coloured("More info is needed to start a quiz.", 'yellow'))
+            elif action.split()[1] == 'french':
+                if action.split()[2] == 'verbs':
+                    quiz.verbs(findSet('french', action.split()[3].lower()))
+                elif action.split()[2] == 'nouns':
+                    quiz.nouns(findSet('french', action.split()[3].lower()))
+            elif action.split()[1] == 'german':
+                if action.split()[2] == 'verbs':
+                    quiz.verbs(findSet('german', action.split()[3].lower()))
+                elif action.split()[2] == 'nouns':
+                    quiz.nouns(findSet('german', action.split()[3].lower()))
 
-    elif action.split()[0] == 'print':
-        print_words_in_set(action.split()[1], action.split()[2])
+        elif action.split()[0] == 'print':
+            print_words_in_set(action.split()[1], action.split()[2])
 
-    elif action == 'save':
-        save_words()
+        elif action == 'save':
+            save_words()
 
-    elif action == 'exit':
-        sys.exit()
+        elif action == 'exit':
+            sys.exit()
+except KeyboardInterrupt:
+    print()
+    sys.exit()
