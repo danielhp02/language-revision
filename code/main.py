661ba2a84a9d527cb50327e164bd08e31bdb3bc1
Daniel <danielhp@optusnet.com.au>
2017-06-27 22:52:29 +1000

    fixed blank line crash

--- code/main.py
+++ code/main.py
@@ -154,47 +154,48 @@ running = True
 try:
     while running:
         action = input(inout.coloured("What would you like to do? ", 'blue', True)).lower()
-        if action == 'add word':
-            english = input(inout.coloured("What is the word in English? ", 'magenta', True)).lower()
-            language = input(inout.coloured("What language? ", 'magenta', True)).lower()
-            translation = input(inout.coloured("What is the word in " + language + "? ", 'magenta', True)).lower()
-            topic = input(inout.coloured("What topic is this word in? ", 'magenta', True)).lower()
+        if len(action) > 0:
+            if action == 'add word':
+                english = input(inout.coloured("What is the word in English? ", 'magenta', True)).lower()
+                language = input(inout.coloured("What language? ", 'magenta', True)).lower()
+                translation = input(inout.coloured("What is the word in " + language + "? ", 'magenta', True)).lower()
+                topic = input(inout.coloured("What topic is this word in? ", 'magenta', True)).lower()
 
-            type = input(inout.coloured("What type of word is this? (noun/verb) ", 'magenta', True)).lower()
-            if type == 'noun':
-                gender = input(inout.coloured("What is the gender of this noun? ", 'magenta', True)).lower()
+                type = input(inout.coloured("What type of word is this? (noun/verb) ", 'magenta', True)).lower()
+                if type == 'noun':
+                    gender = input(inout.coloured("What is the gender of this noun? ", 'magenta', True)).lower()
 
-                add_noun(english, language, translation, gender, topic)
-            elif type == 'verb':
-                pastParticiple = input(inout.coloured("What is the past participle of this verb? ", 'magenta', True)).lower()
-                auxVerb = input(inout.coloured("What auxiliary verb does this verb use? ", 'magenta', True)).lower()
+                    add_noun(english, language, translation, gender, topic)
+                elif type == 'verb':
+                    pastParticiple = input(inout.coloured("What is the past participle of this verb? ", 'magenta', True)).lower()
+                    auxVerb = input(inout.coloured("What auxiliary verb does this verb use? ", 'magenta', True)).lower()
 
-                add_verb(english, language, translation, pastParticiple, auxVerb, topic)
+                    add_verb(english, language, translation, pastParticiple, auxVerb, topic)
 
-        elif action.split()[0] == 'quiz': # needs language, type and topic
-            if len(action.split()) in [1,2,3]:
-                print(inout.coloured("More info is needed to start a quiz.", 'yellow'))
+            elif action.split()[0] == 'quiz': # needs language, type and topic
+                if len(action.split()) < 4:
+                    print(inout.coloured("More info is needed to start a quiz.", 'yellow'))
 
-            elif action.split()[1] == 'french':
-                if action.split()[2] == 'verbs':
-                    quiz.verbs(findSet('french', action.split()[3].lower(), quiet))
-                elif action.split()[2] == 'nouns':
-                    quiz.nouns(findSet('french', action.split()[3].lower(), quiet))
+                elif action.split()[1] == 'french':
+                    if action.split()[2] == 'verbs':
+                        quiz.verbs(findSet('french', action.split()[3].lower(), quiet))
+                    elif action.split()[2] == 'nouns':
+                        quiz.nouns(findSet('french', action.split()[3].lower(), quiet))
 
-            elif action.split()[1] == 'german':
-                if action.split()[2] == 'verbs':
-                    quiz.verbs(findSet('german', action.split()[3].lower(), quiet))
-                elif action.split()[2] == 'nouns':
-                    quiz.nouns(findSet('german', action.split()[3].lower(), quiet))
+                elif action.split()[1] == 'german':
+                    if action.split()[2] == 'verbs':
+                        quiz.verbs(findSet('german', action.split()[3].lower(), quiet))
+                    elif action.split()[2] == 'nouns':
+                        quiz.nouns(findSet('german', action.split()[3].lower(), quiet))
 
-        elif action.split()[0] == 'print': # language, topic
-            print_words_in_set(action.split()[1], action.split()[2])
+            elif action.split()[0] == 'print': # language, topic
+                print_words_in_set(action.split()[1], action.split()[2])
 
-        elif action == 'save':
-            save_words()
+            elif action == 'save':
+                save_words()
 
-        elif action == 'exit':
-            sys.exit()
+            elif action == 'exit':
+                sys.exit()
 except KeyboardInterrupt:
     print()
     sys.exit()
